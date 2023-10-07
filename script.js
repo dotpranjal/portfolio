97ed34efd3673c164ef616d0f71c44b10d3d38b1
dotpranjal <118109086+dotpranjal@users.noreply.github.com>
2023-10-07 18:31:21 +0530

    Second commit about page
    about page just formed and used some js to show the type effect on the main page. also applied animations to the first page.

---
+++ script.js
@@ -0,0 +1,8 @@
+// import typed from "typed.js";
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Machine Learner", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
